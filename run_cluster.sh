c56b165744ee1dd525f6ab41da19371b9bfdf411
remomomo <remomomonti@gmail.com>
2021-12-23 17:14:57 +0100

    Revert "Sprspipe"

--- run_cluster.sh
+++ run_cluster.sh
@@ -5,18 +5,19 @@
 #SBATCH --partition=vcpu
 #SBATCH --time=48:00:00
 #SBATCH -c 1
-#SBATCH --mem 4000
-
-# Initialize conda:
-eval "$(conda shell.bash hook)"
+#SBATCH --mem 2000
 
 SNAKEMAKE_ENV='snakemake'
 
+# Initialize conda:
+eval "$(conda shell.bash hook)"
+# activate snakemake environment 
 conda activate ${SNAKEMAKE_ENV}
 
 snakemake --snakefile workflow/Snakefile \
           --configfile config/config.yaml \
-          --profile ./slurm \
+	  --profile ./slurm \
           --directory "${PWD}" \
-          "${@}"
+	  "${@}"
+
 
