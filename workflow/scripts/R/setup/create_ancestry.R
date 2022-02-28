2090bb00155a459a2ece728c19ab3d6b1da68a46
remomomo <remomomonti@gmail.com>
2022-02-28 10:11:36 +0100

    Update create_ancestry.R
    removed Geno_1KG_dir variable from script.

--- workflow/scripts/R/setup/create_ancestry.R
+++ workflow/scripts/R/setup/create_ancestry.R
@@ -10,27 +10,27 @@
 # Read in environmental variables
 source('workflow/scripts/R/source_config.R')
 
-pop_data<-read.table(paste0(Geno_1KG_dir, '/integrated_call_samples_v3.20130502.ALL.panel'), header=T, stringsAsFactors=F)
+pop_data<-read.table(paste0('resources/1kg/integrated_call_samples_v3.20130502.ALL.panel'), header=T, stringsAsFactors=F)
 
 for(i in unique(pop_data$pop)){
-  write.table(cbind(pop_data$sample[pop_data$pop == i],pop_data$sample[pop_data$pop == i]), paste0(Geno_1KG_dir,'/keep_files/',i,'_samples.keep'), col.names=F, row.names=F, quote=F)
+  write.table(cbind(pop_data$sample[pop_data$pop == i],pop_data$sample[pop_data$pop == i]), paste0('resources/1kg/keep_files/',i,'_samples.keep'), col.names=F, row.names=F, quote=F)
 }
 
 for(i in unique(pop_data$super_pop)){
-  write.table(cbind(pop_data$sample[pop_data$super_pop == i],pop_data$sample[pop_data$super_pop == i]), paste0(Geno_1KG_dir,'/keep_files/',i,'_samples.keep'), col.names=F, row.names=F, quote=F)
+  write.table(cbind(pop_data$sample[pop_data$super_pop == i],pop_data$sample[pop_data$super_pop == i]), paste0('resources/1kg/keep_files/',i,'_samples.keep'), col.names=F, row.names=F, quote=F)
 }
 
 # Create a file listing all ancestry groups
-write.table(unique(pop_data$super_pop), paste0(Geno_1KG_dir,'/super_pop.list'), col.names=F, row.names=F, quote=F)
-write.table(unique(pop_data$pop), paste0(Geno_1KG_dir,'/pop.list'), col.names=F, row.names=F, quote=F)
+write.table(unique(pop_data$super_pop), paste0('resources/1kg/super_pop.list'), col.names=F, row.names=F, quote=F)
+write.table(unique(pop_data$pop), paste0('resources/1kg/pop.list'), col.names=F, row.names=F, quote=F)
 
 # Create a file listing the code of each population and the location of the keep file
 pop_keep_loc<-data.frame(pop=unique(pop_data$pop))
-pop_keep_loc$keep<-paste0(Geno_1KG_dir,'/keep_files/',pop_keep_loc$pop,'_samples.keep')
+pop_keep_loc$keep<-paste0('resources/1kg/keep_files/',pop_keep_loc$pop,'_samples.keep')
 
 super_pop_keep_loc<-data.frame(pop=unique(pop_data$super_pop))
-super_pop_keep_loc$keep<-paste0(Geno_1KG_dir, '/keep_files/',super_pop_keep_loc$pop,'_samples.keep')
+super_pop_keep_loc$keep<-paste0('resources/1kg/keep_files/',super_pop_keep_loc$pop,'_samples.keep')
 
-write.table(super_pop_keep_loc, paste0(Geno_1KG_dir,'/super_pop_keep.list'), col.names=F, row.names=F, quote=F)
-write.table(pop_keep_loc, paste0(Geno_1KG_dir,'/pop_keep.list'), col.names=F, row.names=F, quote=F)
-write.table(rbind(super_pop_keep_loc,pop_keep_loc), paste0(Geno_1KG_dir,'/super_pop_and_pop_keep.list'), col.names=F, row.names=F, quote=F)
+write.table(super_pop_keep_loc, paste0('resources/1kg/super_pop_keep.list'), col.names=F, row.names=F, quote=F)
+write.table(pop_keep_loc, paste0('resources/1kg/pop_keep.list'), col.names=F, row.names=F, quote=F)
+write.table(rbind(super_pop_keep_loc,pop_keep_loc), paste0('resources/1kg/super_pop_and_pop_keep.list'), col.names=F, row.names=F, quote=F)
